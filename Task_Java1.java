20a504f86941bb33379605f641492bb3ff4a2056
ghaniasr22 <95557955+gtfrombubble@users.noreply.github.com>
2022-04-08 21:48:25 +0700

    Biodata
    Task Java 1
    
    Menggunakan Java buatlah program sebagai berikut:
    
    1. Buatlah class dengan Nama Biodata
    2. Buatlah atribut-atribut pada class Biodata sebagai berikut:
        1. Nama lengkap
        2. tempat lahir
        3. tanggal lahir
        4. hobi
        5. zodiak
        6. makanan favorit
        7. cita-cita
    3. Buatlah fungsi-fungsi yang akan mengembalikan data tersebut dalam bentuk string. Contoh: getFullName() akan mengembalikan nama lengkap.
    4. Pada main class, panggil masing-masing atribut pada class Biodata dan cetak setiap atribut tersebut.

---
+++ Task_Java1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.task_java1;
+
+/**
+ *
+ * @author fiqa
+ */
+public class Task_Java1 {
+
+    public static void main(String[] args) {
+        Biodata biodata = new Biodata();
+        
+        biodata.getFullName("Ghania Shafiqa Raisa");
+        biodata.getPlaceBirth("Jakarta");
+        biodata.getDateBirth(22," September ",2003);
+        biodata.getHobby("Photography");
+        biodata.getZodiac("Virgo");
+        biodata.getFavFood("Toast");
+        biodata.getDreams("Software Engineer");
+              
+    }
+}
